ec7c39a4617f0217ddfc38db1d7e8644bda4c1f2
rwqwqrwq-cloud <rwqwqrwq-cloud@gmail.com>
2026-09-17 11:58:57 +0700

    Configure static export and optimize page.tsx for static build

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-﻿import { cookies } from "next/headers";
 import ClientShell from "./components/ClientShell";
 
 export default function Home() {
-  const hasViewedIntro = cookies().get("viewedIntro")?.value === "1";
-  return <ClientShell hasViewedIntro={hasViewedIntro} />;
+  return <ClientShell hasViewedIntro={false} />;
 }
